1b7bcf039ee8bb9131441453adc71f4fa4f30923
Brian Zanolio <zanoliobrian@gmail.com>
2023-02-13 19:53:20 -0300

    terminando el proyecto

--- cv.js
+++ cv.js
@@ -1,4 +1,9 @@
+
 document.getElementById('boton').onclick = function () {
-    document.getElementById("demo1").innerHTML = "Lo que quiero que se muestre al hacer click en el boton"
+    document.getElementById("demo1").innerHTML = "En mis años de experiencia adquirí gran conocimiento que me permite ser muy eficaz en mi trabajo, logrando realizar la reparación de manera efectiva y en un corto plazo de tiempo. Me enfoco en aprender y mejorar dia a dia. Siempre estoy capacitándome para satisfacer las necesidades de los clientes."
     
+}
+
+document.getElementById('boton-fotos').onclick = function () {
+    document.getElementById('trabajos_realizados').style.display='block'
 }
